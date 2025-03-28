8228c0863235a31423ca429904c55918aeb0193f
tnunay <40408057+tnunay@users.noreply.github.com>
2025-03-28 21:53:47 +0700

    Use posetamped instead

--- pb_perception/scripts/retrieve_database.py
+++ pb_perception/scripts/retrieve_database.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
-from geometry_msgs.msg import Point  # Import Point message type
+from geometry_msgs.msg import PointStamped  # Import PointStamped message type
 from pb_perception.palmtree_db import PalmTreeDB  # Import the database module
 from example_interfaces.srv import AddTwoInts  # Use AddTwoInts service to handle integer tag_id
 
@@ -9,10 +9,10 @@ class RetrieveTreePositionNode(Node):
     def __init__(self):
         super().__init__('retrieve_tree_position_node')
         self.db = PalmTreeDB("/home/palmbee1/palmbee_ws/src/PalmBee/pb_perception/database/test.db")
-        self.publisher = self.create_publisher(Point, 'palmbee/tree/position', 10)
+        self.publisher = self.create_publisher(PointStamped, 'palmbee/tree/target_position', 10)
         self.service = self.create_service(AddTwoInts, 'palmbee/tree/request_position', self.handle_service_request)
         self.tag_id = None  # Initialize tag_id as None
-        self.timer = self.create_timer(1.0, self.timer_callback)  # Timer to check and publish
+        self.timer = self.create_timer(0.5, self.timer_callback)  # Timer to check and publish
 
     def handle_service_request(self, request, response):
         self.tag_id = request.a  # Use the first integer (a) as the tag_id
@@ -24,12 +24,14 @@ class RetrieveTreePositionNode(Node):
         if self.tag_id is not None:  # Only publish if a valid tag_id is set
             tree_data = self.db.get_tree(str(self.tag_id))
             if tree_data:
-                point = Point()
-                point.x = tree_data['tag_x']
-                point.y = tree_data['tag_y']
-                point.z = tree_data['tag_z']
-                self.publisher.publish(point)
-                self.get_logger().info(f"Position of tag_id {self.tag_id}: ({point.x}, {point.y}, {point.z})")
+                point_stamped = PointStamped()
+                point_stamped.header.stamp = self.get_clock().now().to_msg()  # Add timestamp
+                point_stamped.header.frame_id = "map"  # Set frame ID to "map"
+                point_stamped.point.x = tree_data['tag_x']
+                point_stamped.point.y = tree_data['tag_y']
+                point_stamped.point.z = tree_data['tag_z']
+                self.publisher.publish(point_stamped)
+                self.get_logger().info(f"Published position for tag_id {self.tag_id}: ({point_stamped.point.x}, {point_stamped.point.y}, {point_stamped.point.z})")
             else:
                 self.get_logger().warn(f"No data found for tag_id {self.tag_id}")
 
